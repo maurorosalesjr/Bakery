26e37c48dc26f13400579ebfa1efedebc593f259
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-06-30 15:11:05 -0700

    start of UI, WIP. marking end of coding session

--- Pierre/Program.cs
+++ Pierre/Program.cs
@@ -7,4 +7,25 @@ using System.Linq;
 class Program
 {
   static void Main()
+  {
+
+    Console.WriteLine("Welcome To Pierre's Bakery!");
+    Console.WriteLine("Can I get you any tasty treats today? ['Y' for Yes, 'N' for No");
+    string answer = Console.ReadLine();
+
+    if (answer == "Y" || answer == "y")
+    {
+      ShowMenu();
+    }
+
+
+  }
+static void ShowMenu()
+{
+  Console.WriteLine("_^_^_^_^_^_^_^_Menu_^_^_^_^_^_^_^_");
+  Console.WriteLine("< Bread                Pastries >");
+  Console.WriteLine("< 1 for $5             1 for $2 >");
+  Console.WriteLine("< 3 for $10            3 for $5 >");
+  Console.WriteLine("_################################_");
+}
 }
