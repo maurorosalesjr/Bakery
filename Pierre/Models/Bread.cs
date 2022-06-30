caa4356a7dc4cacb819eaabad3d99319790d5ebe
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-06-30 11:14:04 -0700

    pre test code

---
+++ Pierre/Models/Bread.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System;
+
+namespace Pierre.Models
+{
+  public class Bread
+  {
+    public int Price { get; set; }
+    public Bread()
+    {
+      Price = 5;
+    }
+  }
+}
