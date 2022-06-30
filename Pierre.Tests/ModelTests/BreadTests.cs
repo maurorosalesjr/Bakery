4fa89e47afb57f8696fa04b8e81ecad1467689bc
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-06-30 11:59:42 -0700

    first test, failure

--- Pierre.Tests/ModelTests/BreadTests.cs
+++ Pierre.Tests/ModelTests/BreadTests.cs
@@ -1,12 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList.Models;
+using Pierre.Models;
 using System.Collections.Generic;
 using System;
+//using Bread;
 
 namespace Pierre.Tests 
 {
-  [TestCLass]
+  [TestClass]
+  public class BreadTests
   {
     [TestMethod]
+    public void Bread_GetBreadPrice_True()
+    {
+      int Price = 5;
+      Bread newBread = new Bread();
+      Assert.AreEqual(true, newBread.Price, Price.ToString());
+    }
   }
-}
+};
